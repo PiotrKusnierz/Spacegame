1575b645dc583a23ec709bc44cfaf5ca79937c63
Sebastian Jarsve <sebastianjarsve@gmail.com>
2018-02-28 10:47:57 +0100

    new file:   Menu.java

---
+++ Game-Project-IntelliJ/src/Menu.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Menu {
+    private ArrayList<String> choices;
+
+    public Menu() {
+        choices = new ArrayList<String>();
+    }
+
+    public void draw() {
+
+    }
+
+    public String selectChoice(String choice) {
+
+    }
+
+    public ArrayList<String> getChoices() {
+        return choices;
+    }
+}
