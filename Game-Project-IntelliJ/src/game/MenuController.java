6a2b942a57914f4cfa31b8388fc4b9d6a192eef1
PiotrKusnierz <piotr.kusnierz@interia.eu>
2018-04-22 20:13:17 +0200

    enabled switching between menu and game scenes

--- Game-Project-IntelliJ/src/game/MenuController.java
+++ Game-Project-IntelliJ/src/game/MenuController.java
@@ -3,34 +3,37 @@ package game;
 import game.views.MenuView;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+
 public class MenuController extends Application {
     public MenuView menuView;
     public Button startGame;
 
 
-
-
-    public void startGame() {
-        //gameState = PLAYING;
+    public void startGame(javafx.event.ActionEvent actionEvent) throws Exception {
         System.out.println("yo");
+        Parent game_page_parent = FXMLLoader.load(getClass().getResource("GameInterface.fxml"));
+        Scene game_page_scene = new Scene(game_page_parent);
+        Stage game_stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+        game_stage.setScene(game_page_scene);
+        game_stage.show();
     }
 
-
-
     @Override
-    public void start(Stage stage) throws Exception{
+    public void start(Stage stage) throws Exception {
         Pane root = FXMLLoader.load(this.getClass().getResource("MenuInterface.fxml"));
         menuView = new MenuView(root);
         stage.setScene(new Scene(root, Color.YELLOW));
         stage.show();
         stage.setTitle("SPACESHIT");
-
-
     }
 }
