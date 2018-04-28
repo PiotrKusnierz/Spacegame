08c86c268e9a5d25f77682c1109d6743fff32b79
PiotrKusnierz <piotr.kusnierz@interia.eu>
2018-04-28 21:57:44 +0200

    improved loading method (fixed crashing problem)

--- Game-Project-IntelliJ/src/game/GameController.java
+++ Game-Project-IntelliJ/src/game/GameController.java
@@ -323,22 +323,28 @@ public class GameController extends Application {
             }
         });
 
-        //[S] [P] Associates clicked button to the right method - loadGame
-        menuView.loadButton.addEventFilter(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                loadGame();
-                gameView.lives.setText("LIVES: " + Integer.toString(game.player.lives));
-                gameView.score.setText("SCORE: " + Integer.toString(game.score));
-                menuView.pausedMenuBox.setVisible(!menuView.pausedMenuBox.isVisible());
-                messageView.removeMessage();
-                gameState = PLAYING;
-
-                File f = new File("./game.sav");               // [P]
-                if(f.exists()) {                                        // [P]
-                    messageView.showAnimatedMessage("LOADED");     // [P]
-                } else {                                                // [P]
-                    messageView.showAnimatedMessage("ERROR");      // [P]
+        // [S] [P] Associates clicked button to the right method - loadGame
+        menuView.loadButton.addEventFilter(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() { // [P]
+            @Override                                                                                 // [P]
+            public void handle(MouseEvent event) {                                                    // [P]
+                File f = new File("./game.sav");                                             // [P]
+                if(f.exists()) {                                                                      // [P]
+                    loadGame();                                                                       // [P]
+                    gameView.lives.setText("LIVES: " + Integer.toString(game.player.lives));          // [P]
+                    gameView.score.setText("SCORE: " + Integer.toString(game.score));                 // [P]
+                    menuView.pausedMenuBox.setVisible(!menuView.pausedMenuBox.isVisible());           // [P]
+                    messageView.removeMessage();                                                      // [P]
+                    gameState = PLAYING;                                                              // [P]
+                    messageView.showAnimatedMessage("LOADED");                                   // [P]
+                } else {                                                                              // [P]
+                    if (gameState == GAMEOVER) {                                                      // [P]
+                        gameState = GAMEOVER;                                                         // [P]
+                        System.out.println("File with saves is not existing!");                       // [P]
+                    } else {                                                                          // [P]
+                        messageView.showAnimatedMessage("ERROR");                                 // [P]
+                        resumeGame(event);                                                            // [P]
+                        System.out.println("File with saves is not existing!");                       // [P]
+                    }
                 }
             }
         });
