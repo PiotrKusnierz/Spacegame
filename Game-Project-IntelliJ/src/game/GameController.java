ca7efb49cf28f7d53fbc9c74068778bdd187515a
PiotrKusnierz <piotr.kusnierz@interia.eu>
2018-05-02 17:20:53 +0200

    deleted AdioPlayer class and all externall libaries, added new method for playing sound - playSound()

--- Game-Project-IntelliJ/src/game/GameController.java
+++ Game-Project-IntelliJ/src/game/GameController.java
@@ -11,6 +11,8 @@ import javafx.animation.AnimationTimer;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.input.MouseEvent;
 import javafx.event.EventHandler;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 import javafx.stage.Screen;
 import javafx.scene.Scene;
@@ -67,6 +69,13 @@ public class GameController extends Application {
         }
     }
 
+    // [P] Method creates one object of type Media which downloads sound files, and one object if type
+    //     MediaPlayer which plays those sound files.
+    public void playSound(String filename) {                                            // [P]
+        Media sound  = new Media(getClass().getResource(filename).toString());           // [P]
+        MediaPlayer mediaPlayer = new MediaPlayer(sound);                                // [P]
+        mediaPlayer.play();                                                              // [P]
+    }
 
     public void saveGame() {
         FileOutputStream fout = null;
@@ -267,11 +276,14 @@ public class GameController extends Application {
         });
     }
 
+
     // [P] Starts the game on onMouseClicked event when "Start Game" options from the menu is chosen
     public void startGame(MouseEvent mouseEvent) throws Exception{                             // [P]
         Pane root = FXMLLoader.load(this.getClass().getResource("GameInterface.fxml"));  // [P]
         Stage game_stage = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();     // [P]
         game_stage.setScene(new Scene(root, Color.BLACK));
+
+        playSound("sound/sound1.mp3");       // sound for testing [P]
 
         gameState = PLAYING;
         menuView = new MenuView(root);
