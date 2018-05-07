a6e8acf56bf78b7de295c967d4089be0ab8575a4
PiotrKusnierz <piotr.kusnierz@interia.eu>
2018-05-07 15:58:48 +0200

    Fixed java Doc for ImageLoader class.

--- Game-Project-IntelliJ/src/game/tools/ImageLoader.java
+++ Game-Project-IntelliJ/src/game/tools/ImageLoader.java
@@ -11,35 +11,34 @@ import javafx.scene.image.Image;
  * @author Sebastian
  */
 public class ImageLoader {
-	/**
-	 * Loads an image and returns it as a Image object.
-	 * @param filePath string value of the image file location.
-	 * @return Image
-	 */
-	public static Image load(String file) {
-		try {
-			file = ImageLoader.class.getResource("../images/"+file).getPath();
-			FileInputStream fis = new FileInputStream(file);
-			Image image = new Image(fis);
-			fis.close();
-			return image;
-		} catch(Exception e) {
-			e.printStackTrace();
-		} 
-		return null;
-	}
+    /**
+     * Loads an image and returns it as a Image object.
+     * @param file string value of the image file location.
+     * @return Image
+     */
+    public static Image load(String file) {
+        try {
+            file = ImageLoader.class.getResource("../images/"+file).getPath();
+            FileInputStream fis = new FileInputStream(file);
+            Image image = new Image(fis);
+            fis.close();
+            return image;
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
-	/**
-	 * Returns an List of Image objects.
-	 * @param files List<String> with image file paths.
-	 * @return List<Image>
-	 */
-	public static List<Image> load(List<String> files) {
-		List<Image> images = new ArrayList<Image>();
-		for (String file : files) {
-			images.add(load(file));
-		}
-		return images;
-	}
+    /**
+     * Returns an List of Image objects.
+     * @param files List<String> with image file paths.
+     * @return List<Image>
+     */
+    public static List<Image> load(List<String> files) {
+        List<Image> images = new ArrayList<Image>();
+        for (String file : files) {
+            images.add(load(file));
+        }
+        return images;
+    }
 }
-
