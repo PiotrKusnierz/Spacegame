1a448640941205b040bb3f3cf54ff7206940740d
PiotrKusnierz <piotr.kusnierz@interia.eu>
2018-05-07 16:57:12 +0200

    Java Docs for MenuView class.

--- Game-Project-IntelliJ/src/game/views/MenuView.java
+++ Game-Project-IntelliJ/src/game/views/MenuView.java
@@ -1,47 +1,48 @@
 package game.views;
 
-
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
 /**
- *  [P] Class holds variables which belongs to the menues.
+ * Class holds variables which belongs to the menues.
+ * @author Piotr Kusnierz
  */
-
 public class MenuView {
-    public VBox pausedMenuBox;   // [P]
-    public VBox mainMenuBox;     // [P]
-    public VBox controlsMenuBox; // [P]
-    public Text resumeButton;    // [P]
-    public Text menuButton;      // [P]
-    public Text saveButton;      // [P]
-    public Text loadButton;      // [P]
-    public Text restartButton;   // [P]
-    public Text continueButton;  // [P]
-    public Text helpButton;      // [P]
-    public Text goBackButton;    // [P]
+    public VBox pausedMenuBox;
+    public VBox mainMenuBox;
+    public VBox controlsMenuBox;
+    public Text resumeButton;
+    public Text menuButton;
+    public Text saveButton;
+    public Text loadButton;
+    public Text restartButton;
+    public Text continueButton;
+    public Text helpButton;
+    public Text goBackButton;
+    public VBox messageBox;
+    public Text yesButton;
+    public Text noButton;
 
-    public VBox messageBox;      // [P]
-    public Text yesButton;       // [P]
-    public Text noButton;        // [P]
-
-
+    /**
+     * Constructor sets the elements from FXML to the variables.
+     * @param root main node of the game.
+	 * @author Piotr Kusnierz
+     */
     public MenuView(Pane root) {
-        pausedMenuBox = (VBox) root.lookup("#pausedMenuBox");    // [P]
-        mainMenuBox = (VBox) root.lookup("#mainMenuBox");        // [P]
-        controlsMenuBox = (VBox) root.lookup("#controlsMenuBox");// [P]
-
-        resumeButton = (Text) root.lookup("#resumeButton");      // [P]
-        menuButton = (Text) root.lookup("#menuButton");          // [P]
-        saveButton = (Text) root.lookup("#saveButton");          // [P]
-        loadButton = (Text) root.lookup("#loadButton");          // [P]
-        restartButton = (Text) root.lookup("#restartButton");    // [P]
-        continueButton = (Text) root.lookup("#continueButton");  // [P]
-        helpButton = (Text) root.lookup("#helpButton");          // [P]
-        goBackButton = (Text) root.lookup("#goBackButton");      // [P]
-        messageBox = (VBox) root.lookup("#messageBox");          // [P]
-        yesButton = (Text) root.lookup("#yesButton");            // [P]
-        noButton = (Text) root.lookup("#noButton");              // [P]
+        pausedMenuBox = (VBox) root.lookup("#pausedMenuBox");
+        mainMenuBox = (VBox) root.lookup("#mainMenuBox");
+        controlsMenuBox = (VBox) root.lookup("#controlsMenuBox");
+        resumeButton = (Text) root.lookup("#resumeButton");
+        menuButton = (Text) root.lookup("#menuButton");
+        saveButton = (Text) root.lookup("#saveButton");
+        loadButton = (Text) root.lookup("#loadButton");
+        restartButton = (Text) root.lookup("#restartButton");
+        continueButton = (Text) root.lookup("#continueButton");
+        helpButton = (Text) root.lookup("#helpButton");
+        goBackButton = (Text) root.lookup("#goBackButton");
+        messageBox = (VBox) root.lookup("#messageBox");
+        yesButton = (Text) root.lookup("#yesButton");
+        noButton = (Text) root.lookup("#noButton");
     }
 }
