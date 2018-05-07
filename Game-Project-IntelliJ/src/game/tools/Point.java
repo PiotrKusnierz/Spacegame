b5499b3de91cc509bfc43473d2a7283bc77cd1ae
PiotrKusnierz <piotr.kusnierz@interia.eu>
2018-05-07 16:17:56 +0200

    Java Docs for Rect class.

--- Game-Project-IntelliJ/src/game/tools/Point.java
+++ Game-Project-IntelliJ/src/game/tools/Point.java
@@ -3,8 +3,8 @@ package game.tools;
 import java.io.Serializable;
 
 /**
-* Class used for storing position or velocity of an object.
-*/
+ * Class used for storing position or velocity of an object.
+ */
 public class Point implements Serializable {
     public double x, y;
 
@@ -18,28 +18,28 @@ public class Point implements Serializable {
         this.y = y;
     }
 
-	/**
-	 * Creates a Point object with x and y defaults to 0 when no parameter is
-	 * given.
-	 */
+    /**
+     * Creates a Point object with x and y defaults to 0 when no parameter is
+     * given.
+     */
     public Point() {
         this(0, 0);
     }
 
-	/**
-	 * Adds x and y value of another Point object to the current Point object.
-	 * @param p Point object to add to current Point object.
-	 */
+    /**
+     * Adds x and y value of another Point object to the current Point object.
+     * @param p Point object to add to current Point object.
+     */
     public void add(Point p) {
         this.x += p.x;
         this.y += p.y;
     }
 
-	/**
-	 * Overrids Object's toString method, to represent the Point with its
-	 * x and y values.
-	 * @return String
-	 */
+    /**
+     * Overrids Object's toString method, to represent the Point with its
+     * x and y values.
+     * @return String
+     */
     @Override
     public String toString() {
         return String.format("Point(x=%.2f, y=%.2f)", this.x, this.y);
